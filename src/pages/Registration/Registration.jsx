f8635490fbdcfdd04bba5268bc270bbcec484d0f
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-04-07 04:00:22 +0600

    login & registration update

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,25 +1,39 @@
-
+import { MdEmail } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+import { RiLockPasswordLine } from "react-icons/ri";
 
 const Registration = () => {
+  const handleRegistration = e =>{
+    e.preventDefault();
+    // const email = e.target.email.value;
+    // const password = e.target.password.value;
+    // console.log(email,password);
+    const form = new FormData(e.currentTarget);
+    const email = form.get('email');
+    const user = form.get('user');
+    const password = form.get('password');
+    console.log(email,user,password);
+  }
+
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
               <div className="hero-content flex-col lg:flex-row-reverse">
               
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                  <form className="card-body">
-                
+                  <form onSubmit={handleRegistration} className="card-body">
                       <div className="input input-bordered flex items-center gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
-                        <input type="text" className="grow" placeholder="Email" />
+                        <FaUser></FaUser>
+                        <input type="text" className="grow" placeholder="Username" name="user" />
                       </div>
                       <div className="input input-bordered flex items-center gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
-                        <input type="text" className="grow" placeholder="Username" />
+                        <MdEmail></MdEmail>
+                        <input type="text" className="grow" placeholder="Email" name="email" />
                       </div>
+                      
                       <div className="input input-bordered flex items-center gap-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
-                        <input type="password" className="grow" value="password" />
+                        <RiLockPasswordLine></RiLockPasswordLine>
+                        <input type="password" className="grow" placeholder="password" name="password" />
                       </div>
 
                     <div className="form-control mt-6">
